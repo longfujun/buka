64331acd5cab2dde51fb52ead1962248c5809e55
longfujun <835391782@qq.com>
2019-08-22 21:44:49 +0800

    头部

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import './style.scss'
 
 export default (props)=>{
     return (
         <header className="app-header">
-           
-            <h1 className="title">顶部</h1>
-         
+
+            <div className="left">
+                <Link to="/home">
+                    <img src="http://m.buka.cn/static/img/logo_2x.png" alt="" className="lef" />
+                </Link>
+                <span>下载APP</span>
+                <img src="http://m.buka.cn/static/img/research_2x.png" alt="" className="rit" />
+            </div>
+
         </header>
     )
 }
---
+++ src/pages/common/Search/Search.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.scss'
+const Search = (props)=>{
+    return (
+        <div className="page subpage" id="search">
+            <div className="conten" >
+                <h1>搜索</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Search;
